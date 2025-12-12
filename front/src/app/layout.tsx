599b8dd6f46d40dec82436fea6f33d8860f0d4f4
goshamartynovich <goshamartynovich@gmail.com>
2025-12-13 00:10:45 +0300

    rename suspended

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,17 +1,17 @@
-import { LoadingServisex } from "@/components";
-import { cn } from "@/lib/utils";
-import { Providers } from "@/providers";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import Link from "next/link";
+import { LoadingServisex } from '@/components';
+import { cn } from '@/lib/utils';
+import { Providers } from '@/providers';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Link from 'next/link';
 
-import favicon from "./favicon.ico";
-import "./globals.css";
+import favicon from './favicon.ico';
+import './globals.css';
 
-const inter = Inter({ subsets: ["cyrillic"] });
+const inter = Inter({ subsets: ['cyrillic'] });
 export const metadata: Metadata = {
-  title: "SERVISEX™",
-  description: "All products crafted by hand in Belarus. We take no responsibility for the quality",
+  title: 'SERVISEX™',
+  description: 'All products crafted by hand in Belarus. We take no responsibility for the quality',
   icons: {
     icon: favicon.src,
   },
@@ -19,18 +19,18 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={cn("antialiased", inter.className)}>
+      <body className={cn('antialiased', inter.className)}>
         {/* <Providers>{children}</Providers> */}
-        <StayTunedPlaceholder />
+        <SuspendedPlaceholder />
       </body>
     </html>
   );
 }
 
-const StayTunedPlaceholder: React.FC = () => {
+const SuspendedPlaceholder: React.FC = () => {
   return (
     <html lang="en" className="w-full h-full">
-      <body className={cn("antialiased", inter.className)}>
+      <body className={cn('antialiased', inter.className)}>
         <div className="w-screen h-screen  flex flex-col justify-center items-center">
           <h1 className="text-3xl font-bold text-center mb-6">
             SERVISEX<sup>tm</sup> website is suspended.
